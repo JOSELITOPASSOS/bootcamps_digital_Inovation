d005d8cf42113faa2e2858040734b4231c4dc2e9
unknown <joselitomartinho@gmail.com>
2020-09-02 21:31:23 -0300

    Criado Projeto Inicial

---
+++ collectionsStrams/src/main/java/br/com/estudos/collection/ExampleList.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.estudos.collection;
+
+/**
+ * @author Joselito
+ *
+ */
+public class ExampleList {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
