c85353938964059410a420187c1767530851df95
unknown <joselitomartinho@gmail.com>
2021-02-09 21:53:12 -0300

    add pasta desafio

---
+++ collectionsStrams/src/main/java/br/com/estudos/desafio/ContaCedula.java
@@ -0,0 +1,26 @@
+package br.com.estudos.desafio;
+
+import java.util.Scanner;
+
+public class ContaCedula {
+
+	public static void main(String[] args) {
+//		try {
+//			  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//			  StringTokenizer st = new StringTokenizer(br.readLine()); // Lê a linha de entrada
+//			  int a = Integer.parseInt(st.nextToken());
+//			  System.out.println(a); // Imprime o dado
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		} 
+		
+		
+		
+	    Scanner input = new Scanner(System.in);
+		System.out.println("Teste" + input.next());
+       
+		  
+
+	}
+
+}
